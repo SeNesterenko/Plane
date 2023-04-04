16497d19dd3cf207afe5f62450f02364dab50a05
LemonSqueesy <alexvladhvat98@yandex.ru>
2023-04-04 23:36:01 +0400

    refactor: changed how Plank switches between ND and plank; style: added ground textures; feat: added affect of flying aircraft by moving planks;

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,7 +11,13 @@ public class GameController : MonoBehaviour
     [SerializeField] private float _smoothTime = 0.3f;
 
     [SerializeField] private Plank _plank;
+    [SerializeField] private GameObject _plankGroup1;
+    [SerializeField] private GameObject _plankGroup2;
+    [SerializeField] private GameObject _plankGroup3;
+    [SerializeField] private GameObject _plankGroup4;
+    [SerializeField] private GameObject _plankGroup5;
     
+
     private float _distance = 85;
     private readonly Vector2 _pitchMinMax = new (-40, 85);
     private Vector3 _smoothVelocity = Vector3.zero;
@@ -27,7 +33,13 @@ public class GameController : MonoBehaviour
             {
                 _distance += -mouseScroll;
                 CheckCameraDistance();
-            }   
+            }
+
+            _plankGroup1.transform.Translate(Vector3.back * 3.0f * Time.deltaTime);
+            _plankGroup2.transform.Translate(Vector3.back * 3.0f * Time.deltaTime);
+            _plankGroup3.transform.Translate(Vector3.back * 3.0f * Time.deltaTime);
+            _plankGroup4.transform.Translate(Vector3.back * 3.0f * Time.deltaTime);
+            _plankGroup5.transform.Translate(Vector3.back * 3.0f * Time.deltaTime);
         }
     }
 
--- Assets/Scripts/Plank.cs
+++ Assets/Scripts/Plank.cs
@@ -8,6 +8,15 @@ public class Plank : MonoBehaviour
     
     [SerializeField] private Material _specialMaterial;
     [SerializeField] private Material _normalMaterial;
+
+    [SerializeField] private GameObject _plank;
+    [SerializeField] private GameObject _nd;
+    [SerializeField] private GameObject _plankGroup1;
+    [SerializeField] private GameObject _plankGroup2;
+    [SerializeField] private GameObject _plankGroup3;
+    [SerializeField] private GameObject _plankGroup4;
+    [SerializeField] private GameObject _plankGroup5;
+    
     
     [SerializeField] private Camera _camera;
 
@@ -26,8 +35,10 @@ public class Plank : MonoBehaviour
     private void Awake()
     {
         _renderer = GetComponent<Renderer>();
+        _camera.transform.position = new Vector3(-20.0032f, 11.86634f, 18.94893f);
+        _camera.transform.rotation = Quaternion.Euler(23.3f, 133.45f, 0);
     }
-    
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -35,13 +46,57 @@ public class Plank : MonoBehaviour
             _isSpecialMode = !_isSpecialMode;
             ChangeState();
         }
+        SpawnPlank();
+    }
+
+    private void SpawnPlank()
+    {
+        if (_plankGroup1.transform.position.z <= -60f)
+        {
+            _plankGroup1.transform.position = new Vector3(0, 0, 40);
+        }
+        if (_plankGroup2.transform.position.z <= -60f)
+        {
+            _plankGroup2.transform.position = new Vector3(0, 0, 40);
+        }
+        if (_plankGroup3.transform.position.z <= -60f)
+        {
+            _plankGroup3.transform.position = new Vector3(0, 0, 40);
+        }
+        if (_plankGroup4.transform.position.z <= -60f)
+        {
+            _plankGroup4.transform.position = new Vector3(0, 0, 40);
+        }
+        if (_plankGroup5.transform.position.z <= -60f)
+        {
+            _plankGroup5.transform.position = new Vector3(0, 0, 40);
+        }
+        
     }
 
     private void ChangeState()
     {
         if (!_isSpecialMode)
         {
+            _renderer.enabled = true;
             _renderer.material = _normalMaterial;
+            _nd.gameObject.SetActive(false);
+            _plankGroup1.SetActive(true);
+            _plankGroup2.SetActive(true);
+            _plankGroup3.SetActive(true);
+            _plankGroup4.SetActive(true);
+            _plankGroup5.SetActive(true);
+            foreach (var cloud in _clouds)
+            {
+                cloud.localScale = _cloudsNormalScale;
+            }
+            
+            _gameController.CheckCameraDistance();
+            _plane.gameObject.SetActive(true);
+            _camera.orthographic = false;
+
+
+            /*_renderer.material = _normalMaterial;
             transform.localScale = _plankNormalScale;
             
             foreach (var cloud in _clouds)
@@ -51,11 +106,30 @@ public class Plank : MonoBehaviour
             
             _plane.gameObject.SetActive(true);
             _camera.orthographic = false;
-            _gameController.CheckCameraDistance();
+            _gameController.CheckCameraDistance();*/
         }
         else
         {
+            _renderer.enabled = false;
             _renderer.material = _specialMaterial;
+            _nd.gameObject.SetActive(true);
+            _plane.gameObject.SetActive(false);
+            _plankGroup1.SetActive(false);
+            _plankGroup2.SetActive(false);
+            _plankGroup3.SetActive(false);
+            _plankGroup4.SetActive(false);
+            _plankGroup5.SetActive(false);
+            
+            
+            foreach (var cloud in _clouds)
+            {
+                cloud.localScale = _cloudsSpecialScale;
+            }
+            
+            _camera.transform.position = new Vector3(0, 40, 0);
+            _camera.orthographic = true;
+            _camera.transform.rotation = Quaternion.Euler(90, 0, 0);
+            /*_renderer.material = _specialMaterial;
             _camera.transform.position = new Vector3(0, 10, 0);
             transform.localScale = _plankSpecialScale;
             
@@ -66,7 +140,7 @@ public class Plank : MonoBehaviour
             
             _plane.gameObject.SetActive(false);
             _camera.orthographic = true;
-            _camera.transform.rotation = Quaternion.Euler(90, 0, 0);
+            _camera.transform.rotation = Quaternion.Euler(90, 0, 0);*/
         }
     }
 }
